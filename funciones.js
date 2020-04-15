da9cd129684b70e18aeef0099a384976ebebc055
Gabriel Ordaz <gjordazg@gmail.com>
2020-04-15 15:46:49 -0300

    Se agregaron los elementos en idioma ingles. Se corrigio el estilo. Se agrego la funcionalidad al boton de traduccion. Falta agregar la funcionalidad a los botones de esconder y mostrar las descripciones.

--- funciones.js
+++ funciones.js
@@ -1,22 +1,39 @@
+let box1 = document.getElementsByClassName("down");
+let box2 = document.getElementsByClassName("up");
+
+function downUp() {
+
+    console.log("voooo");
+}
+
 function Traduccion() {
 
     var prueba = document.getElementById("boton");
+    var espanish = document.getElementsByClassName("esp");
+    var english = document.getElementsByClassName("eng");
 
     if (prueba.value == "ENG") {
 
-        var elemIngles = document.getElementById("traducir");
+        for (let i = 0; i < espanish.length; i++) {
+            espanish[i].classList.add("hide");
+        }
 
-        elemIngles.innerHTML = "Skills";
-        
+       for (let i = 0; i < english.length; i++) {
+            english[i].classList.remove("hide");
+        }
+
         prueba.value = "ESP";
+        
+    }else {
 
-    } else {
-
-        var elemEspanol = document.getElementById("traducir");
-
-        elemEspanol.innerHTML = "Habilidades";
-
+         for (let i = 0; i < espanish.length; i++) {
+            espanish[i].classList.remove("hide");   
+        }
+        
+        for (let i = 0; i < english.length; i++) {
+            english[i].classList.add("hide");
+        }
+        
         prueba.value = "ENG";
     }
-
 }
