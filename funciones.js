f71ef33115793271dcc00ec5c199d8603fb4a918
Gabriel Ordaz <gjordazg@gmail.com>
2020-04-16 00:38:10 -0300

    Se agregaron las funcionalidades a las flechas en el archivo js, agregando un keyframe para ayudar con la animacion. Se cambio el boton de idioma de fijo a flotante. Se cambio el estilo de las barras de subtitulos. Se considera finalizado el producto para su publicacion.

--- funciones.js
+++ funciones.js
@@ -1,9 +1,35 @@
-let box1 = document.getElementsByClassName("down");
-let box2 = document.getElementsByClassName("up");
+const box1 = document.getElementsByTagName("i");
+let box2 = document.getElementsByClassName("contenedor");
+
+for (let i = 0; i < box1.length; i++) {
+    box1[i].addEventListener('click', downUp);
+}
 
 function downUp() {
-
-    console.log("voooo");
+    let x;
+    switch(this){
+        case box1[0]:
+            x = box2[0];
+            break;
+        case box1[1]:
+            x = box2[1];
+            break;
+        case box1[2]:
+            x = box2[2];
+            break;
+    }
+    
+    if (this.className == "fas fa-angle-down") {
+            this.classList.add("fa-angle-up");
+            this.classList.remove("fa-angle-down");
+            x.classList.add("show");
+        } else{
+            this.classList.add("fa-angle-down");
+            this.classList.remove("fa-angle-up");
+            x.classList.remove("show");
+        }
+    
+     x = null;    
 }
 
 function Traduccion() {
@@ -18,22 +44,22 @@ function Traduccion() {
             espanish[i].classList.add("hide");
         }
 
-       for (let i = 0; i < english.length; i++) {
+        for (let i = 0; i < english.length; i++) {
             english[i].classList.remove("hide");
         }
 
         prueba.value = "ESP";
-        
-    }else {
 
-         for (let i = 0; i < espanish.length; i++) {
-            espanish[i].classList.remove("hide");   
+    } else {
+
+        for (let i = 0; i < espanish.length; i++) {
+            espanish[i].classList.remove("hide");
         }
-        
+
         for (let i = 0; i < english.length; i++) {
             english[i].classList.add("hide");
         }
-        
+
         prueba.value = "ENG";
     }
 }
